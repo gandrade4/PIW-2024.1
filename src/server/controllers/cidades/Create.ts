905009bd30c5d115f83d7b5fb2b8f5f5938d8ba8
Gabriel Andrade <42943566+gandrade4@users.noreply.github.com>
2024-05-24 20:13:26 -0300

    video 10

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,21 +1,38 @@
 import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
+import * as yup from 'yup';
 
 interface ICidade{
     nome: string
 }
 
+const bodyValidation: yup.Schema<ICidade> = yup.object().shape({
+    nome: yup.string().required().min(3),
+
+})
+
 //aqui eu vou acessar os dados que receberei do front end - faz sentido com login e formulario de alugar quadra por exemplo
 //la no isominia eu criei uma pasta para receber apenas nomes de cidade
 //todas as variveis foram tipadas para não ficarem any
+//instalamos o yup pra nao precisar fazer varios if elses em formulario
 
-export const create = (req: Request<{}, {}, ICidade>, res: Response) =>{
+export const create = async (req: Request<{}, {}, ICidade>, res: Response) =>{
 
+    let validadeData: ICidade | undefined = undefined;
+    
+    try{
+        validadeData = await bodyValidation.validate(req.body);
 
-    if(req.body.nome === undefined){
-        return res.status(StatusCodes.BAD_REQUEST).send('Informe um >nome< válido)')
+    }catch (error){
+        const yupError = error as yup.ValidationError;
+        return res.json({
+            errors:{
+                default: yupError.message,
+            }
+        })
     }
-    console.log(req.body.nome)
+
+    console.log(validadeData)
 
 
     return res.send('Create!');
